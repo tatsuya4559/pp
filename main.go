427d77af0f800f16f247309485f17e561a664a48
tatsuya4559 <tatsuya.k1029@gmail.com>
2024-11-17 10:25:26 +0900

    feat: Print usage

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
+func printUsage() {
+	usage := `pp is a tool to prettify diff output.
+
+Usage: pp < diff.txt
+`
+	fmt.Fprintln(os.Stderr, usage)
+}
+
 func main() {
+	flag.Usage = printUsage
+	flag.Parse()
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
